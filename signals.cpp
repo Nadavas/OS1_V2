57b5aa31d28690dbe30297245ab8ce46619f6742
Nadavas <117817174+Nadavas@users.noreply.github.com>
2024-02-18 15:35:19 +0200

    Update signals.cpp

--- signals.cpp
+++ signals.cpp
@@ -11,41 +11,40 @@ extern int cur_fg_jid;
 void sig_handler(int sig_number){
 	if(sig_number==SIGINT){
 		std::cout << "smash: caught ctrl-C" << std::endl;
-		if (!fg_empty()){	//checking if there is a job on fg
-			if (!kill(cur_fg_pid, SIGKILL)) {
-				std::cout << "smash: process " << cur_fg_pid << " was killed" << std::endl;
-				fg_clean();
+	if (is_fg_exists()) {
+		if (!kill(fg_pid, SIGKILL)) {
+			std::cout << "smash: process " << fg_pid << " was killed" << std::endl;
+			fg_clear();
+		}
+		else {
+			std::perror("smash error: kill failed");
+		}
+	}
+	else {
+		printf("smash > ");
+		fflush(stdout);
+
+	}
+	}
+	else if (sig_number==SIGSTOP){
+			std::cout << "smash: caught ctrl-Z" << std::endl;
+	if (is_fg_exists()) {
+		if (addNewJob(fg_pid, fg_cmd, Stopped,fg_job_id)) {
+			if (!kill(fg_pid, SIGSTOP)) {
+				std::cout << "smash: process " << fg_pid << " was stopped" << std::endl;
+				fg_clear();
 			}
 			else {
 				std::perror("smash error: kill failed");
 			}
 		}
 		else {
-			printf("smash > ");
-			fflush(stdout);
+			std::cerr << "smash error: addNewJob failed\n";
 		}
 	}
-	else if (sig_number==SIGSTOP){
-			std::cout << "smash: caught ctrl-Z" << std::endl;
-			if (!fg_empty()){	//checking if there is a job on fg
-				if (insert_job(cur_fg_pid, cur_fg_cmd, true,cur_fg_jid)) {
-					if (!kill(cur_fg_pid, SIGSTOP)) {
-						std::cout << "smash: process " << cur_fg_pid << " was stopped" << std::endl;
-						fg_clean();
-					}
-					else {
-					std::perror("smash error: kill failed");
-					}
-				}
-				else {
-					std::cerr << "smash error: insert_job failed\n";
-				}
-			}
-			else {
-				printf("smash > ");
-				fflush(stdout);
-			}
-			
+	else {
+		printf("smash > ");
+		fflush(stdout);
 	}
 
 }	
